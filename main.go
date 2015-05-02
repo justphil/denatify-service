49e483135e476e4dcfac2181dabd1afcaacadc49
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2015-05-02 12:39:37 +0200

    shorten var names and add /bust/[...] route

--- main.go
+++ main.go
@@ -11,25 +11,26 @@ import (
 )
 
 func main() {
-	// init the main router
-	router := httprouter.New()
+	// init router
+	r := httprouter.New()
 
 	// add routes
-	router.GET("/", controller.Action((*controllers.LandingPageController).Index))
-	router.GET("/users", controller.Action((*controllers.UsersController).Users))
-	router.GET("/user/new", controller.Action((*controllers.UsersController).NewUser))
-	router.POST("/user/new", controller.Action((*controllers.UsersController).CreateNewUser))
-	router.GET("/users/:userId", controller.Action((*controllers.UsersController).User))
-	router.GET("/users/:userId/delete", controller.Action((*controllers.UsersController).DeleteUser))
-	router.GET("/users/:userId/perform-deletion", controller.Action((*controllers.UsersController).PerformUserDeletion))
-	router.GET("/users/:userId/update", controller.Action((*controllers.UsersController).UpdateUser))
-	router.POST("/users/:userId/perform-update", controller.Action((*controllers.UsersController).PerformUserUpdate))
+	r.GET("/", controller.Action((*controllers.LandingPageController).Index))
+	r.GET("/users", controller.Action((*controllers.UsersController).Users))
+	r.GET("/user/new", controller.Action((*controllers.UsersController).NewUser))
+	r.POST("/user/new", controller.Action((*controllers.UsersController).CreateNewUser))
+	r.GET("/users/:userId", controller.Action((*controllers.UsersController).User))
+	r.GET("/users/:userId/delete", controller.Action((*controllers.UsersController).DeleteUser))
+	r.GET("/users/:userId/perform-deletion", controller.Action((*controllers.UsersController).PerformUserDeletion))
+	r.GET("/users/:userId/update", controller.Action((*controllers.UsersController).UpdateUser))
+	r.POST("/users/:userId/perform-update", controller.Action((*controllers.UsersController).PerformUserUpdate))
+	r.GET("/bust/:username/:project", controller.Action((*controllers.LandingPageController).Index))
 
 	// initialize middlewares (includes negroni.Recovery, negroni.Logging, negroni.Static)
 	n := negroni.Classic()
 
-	// the main router should always be the last handler
-	n.UseHandler(router)
+	// the router should always be the last handler
+	n.UseHandler(r)
 
 	// bind server to interface / port and block
 	n.Run(":" + getPort())
